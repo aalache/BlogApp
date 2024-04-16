2fbf80994e20a9d3f60332dd07d6380961bc4432
aalache <oaalache@gmail.com>
2024-04-16 18:04:21 +0100

    project setup

---
+++ src/main/java/com/blogapp/myblogapp/entities/Categories.java
@@ -0,0 +1,9 @@
+package com.blogapp.myblogapp.entities;
+
+import java.util.ArrayList;
+
+public enum Categories {
+    HEALTH,
+    POLITIC,
+    SPORT,
+}
---
+++ src/main/java/com/blogapp/myblogapp/entities/Visibility.java
@@ -0,0 +1,6 @@
+package com.blogapp.myblogapp.entities;
+
+public enum Visibility {
+    PUBLIC,
+    PRIVATE,
+}
