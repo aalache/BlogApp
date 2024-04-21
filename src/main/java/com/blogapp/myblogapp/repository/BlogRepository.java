f254c5ef812d3f12adb1a0bf1ce1443b85201e19
aalache <oaalache@gmail.com>
2024-04-21 14:58:13 +0100

    Revert "creating repository and dto classes for Blog ,Bloger and comment classes"
    This reverts commit f493701180a671bb4e227d520e44067dbce6df2e.

--- src/main/java/com/blogapp/myblogapp/repository/BlogRepository.java
+++
@@ -1,9 +0,0 @@
-package com.blogapp.myblogapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.blogapp.myblogapp.entities.Blog;
-
-public interface BlogRepository extends JpaRepository<Blog, Long> {
-
-}
--- src/main/java/com/blogapp/myblogapp/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.blogapp.myblogapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.blogapp.myblogapp.entities.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-}
